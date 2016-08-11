2c8497ac16fc4696693b9e88eefcbbfd2782b51d
metamaker <admin@metamaker.pro>
2016-08-11 16:06:58 +0300

    Fixed comment in getArticlesAction

--- backend/src/BlogBundle/Controller/ArticleController.php
+++ backend/src/BlogBundle/Controller/ArticleController.php
@@ -11,18 +11,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController extends FOSRestController
-{/**
- * Get articles list.
- *
- * Query parameters:
- *   [Optional] offset (default = 0): Get articles starting from {offset}
- *   [Optional] limit (default = 0): Get not more than {limit} articles
- *   [Optional] sort-field (default = created_on): By what field to sort articles. Possible values are 'title', 'created_on', 'updated_on'.
- *   [Optional] sort-order (default = asc): Sort order. Possible values are 'asc', 'desc'.
- *   [Optional] only-published (default = true): If true, return only published articles. If false, return all articles. Works only when user has Admin role.
- *
- * @return JSON stringified array with articles
- */
+{
+    /**
+     * Get articles list.
+     *
+     * Query parameters:
+     *   [Optional] offset (default = 0): Get articles starting from {offset}
+     *   [Optional] limit (default = 0): Get not more than {limit} articles
+     *   [Optional] sort-field (default = created_on): By what field to sort articles. Possible values are 'title', 'created_on', 'updated_on'.
+     *   [Optional] sort-order (default = asc): Sort order. Possible values are 'asc', 'desc'.
+     *   [Optional] only-published (default = true): If true, return only published articles. If false, return all articles. Works only when user has Admin role.
+     *
+     * @return JSON stringified array with articles
+     */
     public function getArticlesAction()
     {
         //return new JsonResponse([1,2]);
