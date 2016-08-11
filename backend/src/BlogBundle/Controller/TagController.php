c06e99ce722e1e893c84afbc7f6046b5f9a2741d
metamaker <admin@metamaker.pro>
2016-08-11 16:01:42 +0300

    Defined TagController methods

--- backend/src/BlogBundle/Controller/TagController.php
+++ backend/src/BlogBundle/Controller/TagController.php
@@ -4,10 +4,67 @@ namespace BlogBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
 class TagController extends FOSRestController
 {
-    //
+    /**
+     * Get tags list.
+     *
+     * @return JSON stringified array with tags
+     */
+    public function getTagsAction()
+    {
+        //
+    }
+
+    /**
+     * Create new tag.
+     *
+     * Query parameters:
+     *   tag: JSON stringified Tag entity.
+     *
+     * @return JSON stringified object with request result.
+     */
+    public function postTagsAction()
+    {
+        //
+    }
+
+    /**
+     * Get tag by slug.
+     *
+     * @param $slug string Tag's slug.
+     *
+     * @return JSON stringified Tag object.
+     */
+    public function getTagAction($slug)
+    {
+        //
+    }
+
+    /**
+     * Update tag by slug.
+     *
+     * Query parameters:
+     *   tag: JSON stringified Tag entity.
+     *
+     * @param $slug string Tag's slug.
+     *
+     * @return JSON stringified object with request result.
+     */
+    public function putTagAction($slug)
+    {
+        //
+    }
+
+    /**
+     * Delete tag by slug.
+     *
+     * @param $slug string Tag's slug.
+     *
+     * @return JSON stringified object with request result.
+     */
+    public function deleteTagAction($slug)
+    {
+        //
+    }
 }
